85022b70d27363199ed76b3cf87a5303e003a56d
Marina Rose <maffia.marina@gmail.com>
2021-10-19 12:36:36 -0400

    template test

--- userpage.php
+++ userpage.php
@@ -1,8 +1,6 @@
-<?php $mkdir = mkdir("users/$username");
-$createPublicPage = fopen("users/$username/index.php", 'w');
-$createSubmitPage = fopen("users/$username/submit.php", 'w');
+<?php ob_start(); ?>
 
-fwrite($createPublicPage, '<link rel="stylesheet" href="css/style.css" media="all">
+<link rel="stylesheet" href="css/style.css" media="all">
 </head>
 <div class="topbar"></div>
 <div id="container">
@@ -13,7 +11,7 @@ fwrite($createPublicPage, '<link rel="stylesheet" href="css/style.css" media="al
   </div>
 
 
-    <?php include("../../config.php");
+<?php include("../../../db/guestbook/config.php");
 
 $dir = getcwd();
 $username = basename($dir);
@@ -43,88 +41,6 @@ $username = basename($dir);
 
     ?>
 
-');
-fwrite($createSubmitPage, '<?php include("../../config.php");
+    <?php $template = ob_get_clean(); ?>
 
-$dir = getcwd();
-$username = basename($dir);
-?>
-<?php
-$error_message = "";$success_message = "";
-
-if($_SERVER[\'REQUEST_METHOD\'] == \'POST\') {
-    header(\'Location: index.php\');
-}
-// Submit Comment
-if(isset($_POST["btnComment"])){
-   $nickname = trim($_POST["nickname"]);
-   $email = trim($_POST["email"]);
-   $comment = trim($_POST["comment"]);
-   $dateposted = date("Y-m-d");
-   $gowner = $username;
-
-   // Check fields are empty or not
-   if ($nickname == "" || $email == "" || $comment == ""){
-     $error_message = "Please fill all fields.";
-   }
-
-   // Insert records
-     $insertSQL = "INSERT INTO guestbooks(gowner,dateposted,nickname,email,comment) values(?,?,?,?,?)";
-     $stmt = $con->prepare($insertSQL);
-     $stmt->bind_param("sssss",$gowner, $dateposted, $nickname,$email,$comment);
-     $stmt->execute();
-     $stmt->close();
-
-     $success_message = "Comment posted successfully.";
-     echo $success_message;
-}
-
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Submit Comment</title>
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <!-- Bootstrap JS -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-
-        <div class="col-md-6" >
-
-          <form method="post" action="">
-
-            <h1>Sign <?php echo $username . "\'s" ?> Guestbook</h1>
-
-            <div class="form-group">
-            <div class="form-group">
-              <label for="nickname">Nickname:</label>
-              <input type="text" class="form-control" name="nickname" id="nickname" required="required" maxlength="30">
-            </div>
-            <div class="form-group">
-              <label for="email">Email:</label>
-              <input type="email" class="form-control" name="email" id="email" required="required" maxlength="35">
-            </div>
-            <div class="form-group">
-              <label for="comment">Comment:</label>
-              <textarea class="form-control" name="comment" id="comment" onkeyup="" required="required" maxlength="10000"></textarea>
-            </div>
-
-            <button type="submit" name="btnComment" class="btn btn-default">Submit</button>
-          </form>
-        </div>
-
-     </div>
-    </div>
-  </body>
-</html>')
-
-?>
+    <?php echo $template; ?>
